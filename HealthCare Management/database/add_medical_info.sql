45469624f3c0c37212d70718eaf154f8c9896e30
Neowiseeeeeee <147794496+Neowiseeeeeee@users.noreply.github.com>
2025-05-17 17:19:01 +0800

    Delete HealthCare Management directory

--- HealthCare Management/database/add_medical_info.sql
+++
@@ -1,8 +0,0 @@
-USE docnow_db;
-
--- Add medical information columns to patients table
-ALTER TABLE patients 
-ADD COLUMN blood_type VARCHAR(10),
-ADD COLUMN allergies TEXT,
-ADD COLUMN current_medications TEXT,
-ADD COLUMN medical_history TEXT; 
